cf395df87c691de7564c6f0a913ed8cd9878d4db
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-04-22 20:43:52 +0530

    Created seializers.py and view function

--- DRF1/api/views.py
+++ DRF1/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+from .models import Student
+from .serializers import StudentSerilizers
 
-# Create your views here.
+#model object for single student
+def stu_detail(request):
+    stObj=Student.objects.get(id=1)
+    serializerData=StudentSerilizers(stObj)      # serializer used to convert complex datatype to python datatype
+    # print(serializerData)
+    # print(f'Serialized data {serializerData.data}')
+    json_data=JSONRenderer().render(serializerData.data)
+    # print(f'Json data {json_data}')
+    return HttpResponse(json_data)
