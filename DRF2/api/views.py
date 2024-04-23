608c98cf8327dbbb96538e60b4c15b46c51001dd
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-04-23 21:26:51 +0530

    Created function for inserting data in table

--- DRF2/api/views.py
+++ DRF2/api/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+import io
+from django.http import JsonResponse
+from rest_framework.parsers import  JSONParser
+from serializers import StudentSerilizers
 # Create your views here.
 def stu_create(request):
     if request.method=='POST':
         json_data=request.body
-        
+        stream=io.BytesIO(json_data)
+        python_data=JSONParser().parse(stream)   # convert json data to python data
+        serializer=StudentSerilizers(data=python_data)    # convert python data to complex data
+        if serializer.is_valid():
+            serializer.save()
+            res={'msg':'Data is created '}
+            return JsonResponse(res)
+        return JsonResponse(serializer.errors)
