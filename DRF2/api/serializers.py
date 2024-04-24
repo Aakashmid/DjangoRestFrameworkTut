027aac030fb6793637f8e8029703fc9b8c7a6132
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-04-24 19:49:05 +0530

    Created   myapp.py and sent post request

--- DRF2/api/serializers.py
+++ DRF2/api/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
-
+from .models import Student
 class StudentSerilizers(serializers.Serializer):
     name=serializers.CharField(max_length=50)
     roll=serializers.IntegerField()
     city=serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)     #** used to unpack dictionary key value pair 
--- DRF2/api/views.py
+++ DRF2/api/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 import io
 from django.http import JsonResponse
 from rest_framework.parsers import  JSONParser
-from serializers import StudentSerilizers
+from .serializers import StudentSerilizers
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+@csrf_exempt  # to remove error dur to csrf token not present
 def stu_create(request):
     if request.method=='POST':
-        json_data=request.body
+        json_data=request.body  # json_data -json data pass byt app
         stream=io.BytesIO(json_data)
+        # print(stream)
         python_data=JSONParser().parse(stream)   # convert json data to python data
+        # print(python_data)
         serializer=StudentSerilizers(data=python_data)    # convert python data to complex data
         if serializer.is_valid():
             serializer.save()
