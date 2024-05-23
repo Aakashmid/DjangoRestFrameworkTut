2f5307511e1868f6cde954fc8975c789f604be83
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-05-23 22:09:40 +0530

    Initial commit

--- AuthenticationAndPermissions/PermissionInfunctionBasedView/project/urls.py
+++ AuthenticationAndPermissions/PermissionInfunctionBasedView/project/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 from api import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('studentapi/', views.studentapi),
