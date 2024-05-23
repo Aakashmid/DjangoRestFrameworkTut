021f4e8822cacaa36836d3db55eb05158a5fe2f0
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-05-23 22:41:46 +0530

    generate token for authentication

--- AuthenticationToken/api/views.py
+++ AuthenticationToken/api/views.py
@@ -9,18 +9,23 @@ from rest_framework.authentication import  TokenAuthentication
 # add rest_framework.authtoken  to install app in settings and then migrate table
 
 
-# token authentication 
+# Generate token
 # 1 first way  - by going to admin site and on authtoken model add token
 # 2nd way - By running "python manage.py drf_create_token <username>" command on terminal it will create token or return created token
 # third way - By exposing  an api endpoint 
 # fourth way - By using signals (in this when user registered then give it token and now he can use it for requests)
 # ---------------
 
-
-
 # third way  #
+
+
 #-----------------------
-# this project is example of this 
+# this project is example of this  
+'''
+In this we sendt post request at gettoken endpoint which redirect to   obtain_auth_token(built in view by rest_framework which check authentication credential and then create return auth token if already has then return old token )
+
+syntax of post request by using httpie package  "   http POST http://127.0.0.1:8000/gettoken/ username="badal" password="badal2345678"  "
+'''
 #-----------------------
 
 # For all type of operations
--- AuthenticationToken/project/urls.py
+++ AuthenticationToken/project/urls.py
@@ -1,12 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.authtoken.views import obtain_auth_token
 from api import views
+
 
 router= DefaultRouter()
 router.register('studentapi',views.StudentModelViewSet,basename='Student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('gettoken/',obtain_auth_token)
 
 ]
