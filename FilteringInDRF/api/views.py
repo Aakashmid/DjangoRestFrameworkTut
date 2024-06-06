45cec1fb1c4caef40bf08325809445a784be8b01
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-06-06 20:39:27 +0530

    filltering in DRF

---
+++ FilteringInDRF/api/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .serializers import StudentSerializer
+from .models import Student
+from django_filters.rest_framework.backends import DjangoFilterBackend
+'''Explore yourself for detail '''
+
+
+
+# Create your views here.
+class StudentListView(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    
+    def get_queryset(self):
+        return Student.objects.filter(passby='Smith')
+    
+
+
+
+# Using django-filter package
+
+'''url look like this 'http://127.0.0.1:8000/studentapi/?city=Berlin' '''
+
+ 
+class StudentListView2(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields=['city']
