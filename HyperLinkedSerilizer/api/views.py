31cf2889d9ed99b1118e067a9ec663f87ddb1893
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-06-15 19:23:08 +0530

    HyperLinkedSerializer

--- HyperLinkedSerilizer/api/views.py
+++ HyperLinkedSerilizer/api/views.py
@@ -4,6 +4,8 @@ from .models import Student
 from .serializers import StudentSerializer
 # Create your views here.
 
+
+
 class StudentViewset(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
